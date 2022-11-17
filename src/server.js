d6f1bed4950ab216f955fa613beed93ab0fd3cc2
kdbswo <kdbswo@gmail.com>
2022-11-18 01:42:42 +0900

    on playground

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
